35cfbed62439b5811f558150a5e9edd251da7d1f
Chris Knox <chris.knox@nzherald.co.nz>
2017-08-08 09:35:42 +1200

    version bump

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -3,16 +3,16 @@ require "language/haskell"
 class Insights < Formula
   include Language::Haskell::Cabal
 
-  version_number = "0.3.0.0"
+  version_number = "0.3.2.1"
 
   desc "NZH Insights static site build tool"
   homepage "https://github.com/nzherald/insights"
   url "https://github.com/nzherald/insights/archive/#{version_number}.tar.gz"
-  sha256 "63238fc9678a833503d48a1f9ff8a9db0f30ad3d4c0c86f994a224b6dd75d239"
+  sha256 "c04ee9864d30a099635c5652b348a123c90f28cdb7436b7f5d95c6043e781d6b"
 
   bottle do
     root_url "https://github.com/nzherald/homebrew-nzh/raw/master/Bottles"
-      sha256 "bc71a4b80d649fe61bf60c217a57931838ef506825f82a82b73177ad065794ac" => :sierra
+      sha256 "7ca50cb65a832f3847a31c7b39ac87060faf6ec8e6f541b5293017e9e8459cf9" => :sierra
   end
 
   depends_on "ghc" => :build
