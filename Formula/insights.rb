69bf3b5ed3ad542ec32f275e61adf8df0a233f81
Chris Knox <chris.knox@nzherald.co.nz>
2017-08-08 10:27:39 +1200

    fixed a small bug

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -3,16 +3,16 @@ require "language/haskell"
 class Insights < Formula
   include Language::Haskell::Cabal
 
-  version_number = "0.3.2.1"
+  version_number = "0.3.2.2"
 
   desc "NZH Insights static site build tool"
   homepage "https://github.com/nzherald/insights"
   url "https://github.com/nzherald/insights/archive/#{version_number}.tar.gz"
-  sha256 "c04ee9864d30a099635c5652b348a123c90f28cdb7436b7f5d95c6043e781d6b"
+  sha256 "56cc7f4f53272c73cc5de98234d3b5a4dca12ed8046414d037916f0388f7ca78"
 
   bottle do
     root_url "https://github.com/nzherald/homebrew-nzh/raw/master/Bottles"
-      sha256 "7ca50cb65a832f3847a31c7b39ac87060faf6ec8e6f541b5293017e9e8459cf9" => :sierra
+      sha256 "bb2591d3956420914028b72bebfc762c19cdd352cd1eb444d7ffe57085c0a669" => :sierra
   end
 
   depends_on "ghc" => :build
