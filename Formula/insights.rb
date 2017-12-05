617dbff71c71433b54ad3005171dc172ee302a3c
Chris Knox <chris.knox@nzherald.co.nz>
2017-12-06 12:04:23 +1300

    try to use highsierra as sierra ...

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -13,6 +13,7 @@ class Insights < Formula
   bottle do
     root_url "http://bottles.newsapps.nz"
     sha256 "3d7ba3d93e895b9048833d6389e8db3b6dd4de3dbeccb13c25d532a6fc8c9011" => :sierra
+    sha256 "3d7ba3d93e895b9048833d6389e8db3b6dd4de3dbeccb13c25d532a6fc8c9011" => :high_sierra
   end
 
   depends_on "ghc" => :build
