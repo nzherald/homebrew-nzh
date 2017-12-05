fd56750933bba317a13b15c35746e27c68b823a1
Chris Knox <chris.knox@nzherald.co.nz>
2017-12-06 12:08:57 +1300

    this should work

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -13,7 +13,7 @@ class Insights < Formula
   bottle do
     root_url "http://bottles.newsapps.nz"
     sha256 "3d7ba3d93e895b9048833d6389e8db3b6dd4de3dbeccb13c25d532a6fc8c9011" => :sierra
-    sha256 "3d7ba3d93e895b9048833d6389e8db3b6dd4de3dbeccb13c25d532a6fc8c9011" => :high_sierra
+    sha256 "ab35b6fccf0fa8d06b84fb7f86f243f05db646927cbe7cf6a7c2dbd57cd8084c" => :high_sierra
   end
 
   depends_on "ghc" => :build
