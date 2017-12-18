66fab4d95a6a42a0d5edd7accef0d62299d9a440
Chris Knox <chris.knox@nzherald.co.nz>
2017-12-18 16:26:48 +1300

    0.4.6.1

--- Formula/insights.rb
+++ Formula/insights.rb
@@ -8,11 +8,12 @@ class Insights < Formula
   desc "NZH Insights static site build tool"
   homepage "https://github.com/nzherald/insights"
   url "https://github.com/nzherald/insights/archive/#{version_number}.tar.gz"
-  sha256 "2ee2f31b408a82a13601672c6256192edd90d2ce554c3f92dbaf360a6b36172c"
+  sha256 "71b721fcb59224aa69d44d02e8cf2c107dfe145e45f0c77bebe729af1e7bc5aa"
 
   bottle do
     root_url "http://bottles.newsapps.nz"
-    sha256 "a211c76007c34cfa53ced29af49cd33947e84467d1698b1d3db9a7ac8eb8d448" => :high_sierra
+    sha256 "ecec75250478a685dacd82c3e6f90174bb8b9004b13e0b9c8f1595d518a0974d" => :high_sierra
+    sha256 "d9d638d18f6de84ec14e2e87a437f379f745e05ea2bee5299b0ff39afda1a6c5" => :sierra
   end
 
   depends_on "ghc" => :build
